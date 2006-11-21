0b3e55a0efc9f127c2036fb4d361aa75724b7adb
mdarby <mdarby@16e87739-ad3f-4bb6-a69b-24329e42ab7f>
2006-11-21 22:53:27 +0000

    Initial revision
    git-svn-id: file:///home/subversion/HiQVisual@2 16e87739-ad3f-4bb6-a69b-24329e42ab7f

---
+++ trunk/src/HiQVisual/gamePanel.java
@@ -0,0 +1,19 @@
+package HiQVisual;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JPanel;
+
+public class gamePanel extends JPanel{
+  
+  public gamePanel() {
+    this.setBackground(Color.BLACK);
+  }
+  
+  public void paint(Graphics g) {
+    super.paint(g);
+  }
+}
